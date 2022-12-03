031f7621f3ae91bcb609f9139a36c560689768c6
Iñaki Esteve Arríen <inaki.esteve@mercell.com>
2022-12-03 16:40:14 +0100

    Minor refactor

--- JwtWebApiTutorial/Controllers/AuthController.cs
+++ JwtWebApiTutorial/Controllers/AuthController.cs
@@ -55,8 +55,8 @@ namespace JwtWebApiTutorial.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.Role, "Admin")
+                new(ClaimTypes.Name, user.Username),
+                new(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
